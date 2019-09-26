8c8c455ff050b952411bf717eeb3e2e761e4b5ff
Chtibo <thib_vincent@hotmail.com>
2019-09-26 15:08:02 +0200

    All is done

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 import logging
 #logging.warn(os.environ["DUMMY"])
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,36 +14,58 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
 @app.route('/hello')
 def hello():
+    print("Hello", flush=True)
     return "Hello World!"
 
 @app.route('/api/v1/products')
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    print (products, flush=True)
     return products_schema.jsonify(products)
 
 @app.route('/api/v1/products/<int:id>', methods=["GET"])
 def get_product(id):
-    print (id)
     product = db.session.query(Product).get(id)
-    print(product)
+    print (id, flush=True)
+    print (product, flush=True)
     if product != None:
-        return product
+        return product_schema.jsonify(product), 200
     return '', 404
 
 @app.route('/api/v1/products/<int:id>', methods=["DELETE"])
 def delete_product(id):
-    for product in PRODUCTS:
-        if product["id"] == id :
-            print('delete')
-            PRODUCTS.remove(product)
-            return '', 204
+    product = db.session.query(Product).get(id)
+    print (id, flush=True)
+    print (product, flush=True)
+    if product != None:
+        db.session.delete(product)
+        db.session.commit()
+        return '', 204
     return '', 404
 
-@app.route('/api/v1/products/', methods=["POST"])
+@app.route('/api/v1/products', methods=["POST"])
 def add_products():
-    PRODUCTS.append({"id": 4 ,"name" : "Here"})
+
+    product_name = request.form.get('name')
+    print(product_name, flush=True)
+    product_description = request.form.get('description')
+    print(product_description, flush=True)
+    product = Product(name=product_name,description=product_description)
+    print(product, flush=True)
+    db.session.add(product)
+    db.session.commit()
+    return "", 201
+
+@app.route('/api/v1/products/<int:id>', methods=["PATCH"])
+def update_products(id):
+    product = db.session.query(Product).get(id)
+    print (id, flush=True)
+    print (product, flush=True)
+    product.name = request.form.get('name')
+    print(product, flush=True)
+    db.session.commit()
     return "", 201
