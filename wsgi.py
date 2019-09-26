d9f96e89a3d139c63d79086e35942ead9b3b98da
Chtibo <thib_vincent@hotmail.com>
2019-09-26 12:10:18 +0200

    get_product

--- wsgi.py
+++ wsgi.py
@@ -20,7 +20,29 @@ from schemas import products_schema
 def hello():
     return "Hello World!"
 
-@app.route('/products')
+@app.route('/api/v1/products')
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/api/v1/products/<int:id>', methods=["GET"])
+def get_product(id):
+
+    product = db.query(Product).get(id)
+    if product != None:
+        return jsonify(product)
+    return '', 404
+
+@app.route('/api/v1/products/<int:id>', methods=["DELETE"])
+def delete_product(id):
+    for product in PRODUCTS:
+        if product["id"] == id :
+            print('delete')
+            PRODUCTS.remove(product)
+            return '', 204
+    return '', 404
+
+@app.route('/api/v1/products/', methods=["POST"])
+def add_products():
+    PRODUCTS.append({"id": 4 ,"name" : "Here"})
+    return "", 201
